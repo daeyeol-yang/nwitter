b0e82507cffe51c9ef7f2263eedbe29dcc2593d8
Daeyoul-Yang <cnrrnehfdl03@naver.com>
2023-10-29 20:35:43 +0900

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,69 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Profile from './routes/profile'
+import Home from './routes/home'
+import Layout from './components/layout'
+import CreateAccout from './routes/create-account'
+import Login from './routes/login'
+import CreateAccount from './routes/create-account'
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+import LoadingScreen from './components/loading-screen'
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"",
+        element: <Home/>,
+      },
+      {
+        path: "profile",
+        element: <Profile/>,
+      },
+    ],
+  },
+  {
+    path:"/login",
+    element:<Login />
+  },
+  {
+    path: "/create-account",element:<CreateAccount />
+  }
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 
 function App() {
+  const[isLoading,setLoading] = useState(true);
+  const init = async() =>{
+    setLoading(false);
+  }
+  useEffect(()=> {
+    init();
+  },[]);
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <GlobalStyles />
+    {isLoading?<LoadingScreen/>:<RouterProvider router={router}/>}
 
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBAf8ErEBGME4iEFJHjOvpR4CmU7skZ1k",
+  authDomain: "nwitter-reloaded-b9350.firebaseapp.com",
+  projectId: "nwitter-reloaded-b9350",
+  storageBucket: "nwitter-reloaded-b9350.appspot.com",
+  messagingSenderId: "428070930018",
+  appId: "1:428070930018:web:94555e0a184b4d3e71f33b"
+};
+
+
+const app = initializeApp(firebaseConfig);
